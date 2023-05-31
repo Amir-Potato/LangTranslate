1f518849728f08717407372ebf8f69d89ebeb23c
BakedPotato101 <jackson.baker7562@gmail.com>
2023-05-31 12:32:52 -0500

    Jackson's Languages

---
+++ Rust/helloworld/target/debug/deps/helloworld-a1bb0a59e0226722.d
@@ -0,0 +1,5 @@
+/home/jax/Playground/LangTranslate/Rust/helloworld/target/debug/deps/helloworld-a1bb0a59e0226722: src/main.rs
+
+/home/jax/Playground/LangTranslate/Rust/helloworld/target/debug/deps/helloworld-a1bb0a59e0226722.d: src/main.rs
+
+src/main.rs:
---
+++ Rust/helloworld/target/debug/helloworld.d
@@ -0,0 +1,1 @@
+/home/jax/Playground/LangTranslate/Rust/helloworld/target/debug/helloworld: /home/jax/Playground/LangTranslate/Rust/helloworld/src/main.rs
