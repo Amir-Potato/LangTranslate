7dcc880d55764b2b285b6617a6e3162c49b5f13e
BakedPotato101 <jackson.baker7562@gmail.com>
2023-05-31 13:43:01 -0500

    Rust Solution

--- Rust/twosum/src/main.rs
+++ Rust/twosum/src/main.rs
@@ -1,3 +1,17 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+         for (i, num1) in nums.iter().enumerate() {
+            for (j, num2) in nums.iter().skip(i+1).enumerate() {
+               if num1 + num2 == target {
+                    return vec!(i as i32, (j + 1 + i) as i32);
+                }
+            }
+        }
+        vec![]
+    }
+}
 fn main() {
-    println!("Hello, world!");
+    assert_eq!(Solution::two_sum(vec![2, 7, 11, 15], 9), vec![0,1]);
 }
